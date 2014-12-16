f75c3a1eaa12b42aad7c596a2a59a7b27d422a53
Robert O'Connor <rob@oconnor.ninja>
2014-12-16 04:02:32 -0500

    remove the web server

--- utils/generate_random_values.py
+++ utils/generate_random_values.py
@@ -1,7 +1,7 @@
 import random
 ph = [random.uniform(0, 14) for x in range(300000)]
 temp = [random.uniform(55, 90) for x in range(300000)]
-temp_file = open('/home/rob/aquarimeter-web/readings.csv', 'w+')
+readings_file = open('/home/rob/aquarimeter-web/readings.csv', 'w+')
 for x in range(len(temp)):
-   temp_file.write("%.2f,%.2f\n" % (temp[x],ph[x]))
-temp_file.close()   
+   readings_file.write("%.2f,%.2f\n" % (temp[x],ph[x]))
+readings_file.close()   
