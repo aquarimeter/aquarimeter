88a95ea58d38e281b291796273aac716f02812d0
SI37 <suditw@gmail.com>
2014-12-23 00:27:58 -0500

    fixed intial set_up and aquarimeter

--- aquarimeter.py
+++ aquarimeter.py
@@ -5,6 +5,9 @@ import glob
 import time
 import RPi.GPIO as GPIO
 import picamera
+import json
+import requests
+import sys
 GPIO.setmode(GPIO.BCM)
 DEBUG = 1
 
@@ -15,50 +18,138 @@ os.system('modprobe w1-therm')
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
-#for reading from DS18B20 temperature connected to GPIO4
-def read_temp_raw():
-    f = open(device_file, 'r')
-    lines = f.readlines()
-    f.close()
-    return lines
 
+url = "http://aquarimeter.rocks/api/v1/"
 
-# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
-def readadc(adcnum, clockpin, mosipin, misopin, cspin):
-        if ((adcnum > 7) or (adcnum < 0)):
-                return -1
-        GPIO.output(cspin, True)
- 
-        GPIO.output(clockpin, False)  # start clock low
-        GPIO.output(cspin, False)     # bring CS low
- 
-        commandout = adcnum
-        commandout |= 0x18  # start bit + single-ended bit
-        commandout <<= 3    # we only need to send 5 bits here
-        for i in range(5):
-                if (commandout & 0x80):
-                        GPIO.output(mosipin, True)
-                else:
-                        GPIO.output(mosipin, False)
-                commandout <<= 1
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
- 
-        adcout = 0
-        # read in one empty bit, one null bit and 10 ADC bits
-        for i in range(12):
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
-                adcout <<= 1
-                if (GPIO.input(misopin)):
-                        adcout |= 0x1
- 
-        GPIO.output(cspin, True)
+auth_token = sys.argv[1]
+ideal_T = sys.argv[2]
+minT = sys.argv[3]
+maxT = sys.argv[4]
+namea = "test_tank"
+add_aquarium = {"auth_token":auth_token,
+                "aquarium": {"name":namea,
+                             "ideal_temperature":ideal_T,
+                             "ideal_temp_low":minT,
+                             "ideal_temp_high":maxT}}
+aquarium_data = json.dumps(add_aquarium)
+url_add_aqua = url +"aquariums"
+aquarium_done = requests.post(url_add_aqua, aquarium_data)
+name_aquarium = "mine"
+
+class aquarium(object):
+
+    #send temperature and ph to readings to aquarimeter web
+    def send_readings(self, ph, temp):
+        reading_data = {"auth_token":auth_token,
+                        "reading": {"ph":ph,
+                                    "temperature":temp}}
+        send_read = json.dumps(reading_data)
+        url_send = url + name_aquarium +/readings"
+        sent_data = requests.post(url_send, send_read)
+
+    #recieves update on name and/or temperature
+    def recieve_update(self):
+        update_request = {"auth_token":auth_token,
+                          "aquarium": {"name":"foobar"}}
+        update_get = json.dumps(update_request)
+        url_update = url + name_aquarium
+        update_recieve = requests.patch(url_update,update_get)
         
-        adcout >>= 1       # first bit is 'null' so drop it
-        return adcout
- 
+    #for reading from DS18B20 temperature connected to GPIO4
+    def read_temp_raw(self):
+        f = open(device_file, 'r')
+        lines = f.readlines()
+        f.close()
+        return lines
 
+
+    # read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
+    def readadc(self,adcnum, clockpin, mosipin, misopin, cspin):
+            if ((adcnum > 7) or (adcnum < 0)):
+                    return -1
+            GPIO.output(cspin, True)
+ 
+            GPIO.output(clockpin, False)  # start clock low
+            GPIO.output(cspin, False)     # bring CS low
+ 
+            commandout = adcnum
+            commandout |= 0x18  # start bit + single-ended bit
+            commandout <<= 3    # we only need to send 5 bits here
+            for i in range(5):
+                    if (commandout & 0x80):
+                        GPIO.output(mosipin, True)
+                    else:
+                        GPIO.output(mosipin, False)
+                    commandout <<= 1
+                    GPIO.output(clockpin, True)
+                    GPIO.output(clockpin, False)
+ 
+            adcout = 0
+            # read in one empty bit, one null bit and 10 ADC bits
+            for i in range(12):
+                    GPIO.output(clockpin, True)
+                    GPIO.output(clockpin, False)
+                    adcout <<= 1
+                    if (GPIO.input(misopin)):
+                            adcout |= 0x1
+ 
+            GPIO.output(cspin, True)
+        
+            adcout >>= 1       # first bit is 'null' so drop it
+            return adcout
+
+    #takes picture
+    def takePic(self):
+        with picamera.PiCamera() as camera:
+            camera.capture('/home/pi/Desktop/image.jpg')
+        
+    #checks if anyhing is a good distance away to take a picture
+    def moveSens(self):
+        pir_pot = readadc(pir_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)
+        if pir_pot >150 and pir_pot < 650:
+            takePic()
+
+    #reads current ph
+    def readPh(self):
+        ph_pot = readadc(ph_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)
+        ph_value = ph_pot*5/1024*3.5
+        return ph_value
+
+    #read temperature
+    def read_Temp(self):
+        lines = self.read_temp_raw()
+        while lines[0].strip()[-3:] != 'YES':
+                time.sleep(0.2)
+                lines = read_temp_raw()
+            
+        equals_pos = lines[1].find('t=')
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos+2:]
+            temp_c = float(temp_string) / 1000.0
+            temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_f
+    
+    # turn h bridge to heat
+    def heat_On(self):
+        GPIO.output(A1, GPIO.HIGH)
+        GPIO.output(A2, GPIO.LOW)
+        GPIO.output(A3, GPIO.HIGH)
+        GPIO.output(A4, GPIO.LOW)
+
+    # turn h bridge to cool
+    def cool_On(self):
+        GPIO.output(A1, GPIO.LOW)
+        GPIO.output(A2, GPIO.HIGH)
+        GPIO.output(A3, GPIO.LOW)
+        GPIO.output(A4, GPIO.HIGH)
+
+    #turn off any heating and cooling
+    def peltio_Off(self):
+        GPIO.output(A1, GPIO.LOW)
+        GPIO.output(A2, GPIO.LOW)
+        GPIO.output(A3, GPIO.LOW)
+        GPIO.output(A4, GPIO.LOW)
+    
 # SPI port on the ADC to the Cobbler
 SPICLK = 18
 SPIMISO = 23
@@ -72,90 +163,53 @@ GPIO.setup(SPICLK, GPIO.OUT)
 GPIO.setup(SPICS, GPIO.OUT)
  
 #set up gpio pin for h bridge
-EN12 = 2
+
 A1 = 17
 A2 = 27
-EN34 = 3
-A3 = 10
-A4 = 9
+A3 = 14
+A4 = 15
 
 #set pins up for output for bridge
-GPIO.setup(EN12,GPIO.OUT)
 GPIO.setup(A1,GPIO.OUT)
 GPIO.setup(A2,GPIO.OUT)
-GPIO.setup(EN34,GPIO.OUT)
 GPIO.setup(A3,GPIO.OUT)
 GPIO.setup(A4,GPIO.OUT)
 
-#set up h bridge to turn on 
-GPIO.output(EN12, GPIO.HIGH)
-GPIO.output(EN34, GPIO.HIGH)
+
 #set up values
-ph_adc = 0;
+ph_adc = 2;
 pir_adc = 1;
-ideal_temp = 75;
-max_temp = 80;
-min_temp = 70;
+ideal_temp = ideal_T;
+max_temp = maxT;
+min_temp = minT;
 heat = False;
 cool = False;
-
+aqua = aquarium()
 while True:
-
-        print(read_temp())
-        
         # read the analog pin for ph 
-        ph_pot = readadc(ph_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)
-        ph_value = ph_pot*5/1024*3.5
+        ph_value = aqua.readPh()
 
         #read the analog pin for pir
-        pir_pot = readadc(pir_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)
-        if pir_pot >150 and pir_pot < 650:
-            #maybe if funtion for time to make sure not a lot of pictures is taken and overload pi
-            with picamera.PiCamera() as camera:
-                    camera.capture('/home/pi/Desktop/image.jpg')
-            
-        
+        aqua.moveSens()      
 
         #read temp
-        lines = read_temp_raw()
-        while lines[0].strip()[-3:] != 'YES':
-            time.sleep(0.2)
-            lines = read_temp_raw()
-            
-        equals_pos = lines[1].find('t=')
-        if equals_pos != -1:
-            temp_string = lines[1][equals_pos+2:]
-            temp_c = float(temp_string) / 1000.0
-            temp_f = temp_c * 9.0 / 5.0 + 32.0
-            
+        temp_f = aqua.read_temp()
+        aqua.send_readings(ph_value, temp_f)
         if temp_f < min_temp:
             heat = True
             #turn h bridge to heat
-            GPIO.output(A1, GPIO.HIGH)
-            GPIO.output(A2, GPIO.LOW)
-            GPIO.output(A3, GPIO.HIGH)
-            GPIO.output(A4, GPIO.LOW)
+            aqua.heat_On()
         elif temp_f > max_temp:
             cool = True
             #turn h bridge to cool
-            GPIO.output(A1, GPIO.LOW)
-            GPIO.output(A2, GPIO.HIGH)
-            GPIO.output(A3, GPIO.LOW)
-            GPIO.output(A4, GPIO.HIGH)
+            aqua.cool_On()
         elif heat and temp_f > ideal_temp:
             heat = False
             #turn off heating
-            GPIO.output(A1, GPIO.LOW)
-            GPIO.output(A2, GPIO.LOW)
-            GPIO.output(A3, GPIO.LOW)
-            GPIO.output(A4, GPIO.LOW)
+            aqua.peltio_Off()
         elif cool and temp_f < ideal_temp:
             cool = False
             #turn off cooling
-            GPIO.output(A1, GPIO.LOW)
-            GPIO.output(A2, GPIO.LOW)
-            GPIO.output(A3, GPIO.LOW)
-            GPIO.output(A4, GPIO.LOW)
-
+            aqua.peltio_Off()
         # hang out and do nothing for a half second
         time.sleep(.1)
--- initialsetup.py
+++ initialsetup.py
@@ -5,7 +5,11 @@ pygtk.require('2.0')
 import gtk
 import json
 import requests
+import twisted
+import sys
+import os
 
+url = "http://aquarimeter.rocks/api/v1/"
 class Aquarium_Setup:
 
     #checks required fields is filled before registering
@@ -25,6 +29,16 @@ class Aquarium_Setup:
         else:
             return True
 
+    #checks temperature values are valid 
+    def check_temp(self):
+        idealT = self.ideal_spin.get_value()
+        minT = self.min_spin.get_value()
+        maxT = self.max_spin.get_value()
+        if(minT <= idealT and idealT<= maxT):
+            return True
+        else:
+            return False
+        
     #pop up message box asking to fill all fields
     def error_fill(self):
         info = gtk.MessageDialog(type=gtk.MESSAGE_WARNING, buttons=gtk.BUTTONS_OK)
@@ -41,6 +55,15 @@ class Aquarium_Setup:
         info.run()
         info.destroy()
 
+    #pop up message for bad temperature set
+    def error_temp_range(self):
+        info = gtk.MessageDialog(type=gtk.MESSAGE_WARNING, buttons=gtk.BUTTONS_OK)
+        info.set_property('title', 'Wrong temperature range')
+        info.set_property('text', 'Ideal temperature must be between Min and Max temperature.\n'
+                          'Min temperature must be less than or equal to Max temperature')
+        info.run()
+        info.destroy()
+        
     #pop up message for registeration error code == 422
     def error_register(self):
         info = gtk.MessageDialog(type=gtk.MESSAGE_ERROR, buttons=gtk.BUTTONS_CLOSE)
@@ -48,7 +71,7 @@ class Aquarium_Setup:
         info.set_property('text', 'Something went wrong. is email valid?')
         info.run()
         info.destroy()
-
+        
     #registers with entered information from gui
     def register(self):
         #json payload send happens here
@@ -59,51 +82,60 @@ class Aquarium_Setup:
                                  "password":password,
                                  "first_name":self.first_Name.get_text(),
                                  "last_name":self.last_Name.get_text()}}
-
+        
         reg_data = json.dumps(register_data)
-        url = "some url. need a specific one before sending"
-        reg = requests.post(url, reg_data)
-
+        url_reg = url + "register"
+        reg = requests.post(url_reg, reg_data)
+        
         if reg.status_code == 200:
             self.login(email,password)
         else:
             self.error_register()
-
+            
 
     #login with password and email
     def login(self,email,password):
         #login  happens here
         login_data = {"user":{"email":email,
                                  "password":password}}
-
+        
         log_data = json.dumps(login_data)
-        url = "some url. need a specific one before sending"
-        log = requests.post(url, log_data)
-
+        url_log = url + "login"
+        log = requests.post(url_log, log_data)
+        
         if log.status_code == 200:
             auth_token = log[auth_token]
             #then run aquarimeter sending auth_token to it along with
-            #ideal, min and max temp
+            idealT = self.ideal_spin.get_value()
+            minT = self.min_spin.get_value()
+            maxT = self.max_spin.get_value()
+            comand = "sudo aquarimeter.py" + auth_token + idealT + minT + maxT
+            os.system(comand)
+            
         else:
             print "email/password is incorrect"
-
+            
 
     def check_register(self,widget):
         fill = self.validate_fields()
         long_pwd = self.check_length(self.valid_Password.get_text())
+        temp_valid = self.check_temp()
+
         if(not fill):
             self.error_fill()
         elif(not long_pwd):
             self.error_password()
+        elif(not temp_valid):
+            self.error_temp_range()
         else:
             self.register()
-
+    
     #main gui window
     def __init__(self):
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.window.set_title("Aquarimeter Intial Setup")
         self.window.connect("destroy", lambda wid: gtk.main_quit())
-
+        
         main_Box = gtk.VBox(False, 5) #all items will be arranged vertically in here
         self.window.add(main_Box)
         self.window.set_border_width(10)
@@ -115,7 +147,7 @@ class Aquarium_Setup:
                           "oconnor.ninja/")
         main_Box.pack_start(label)
         #create fields for registeration information to be entered
-        reg_frame = gtk.Frame("Login Details for Aquarimeter Web")
+        reg_frame = gtk.Frame("Login Details for Aquarimeter Web") 
 
         #labels for information needed
         info_Box = gtk.HBox(False,5)
@@ -134,19 +166,19 @@ class Aquarium_Setup:
         #fields to enter information
         vbox2 = gtk.VBox(False,5)
         info_Box.add(vbox2)
-
+        
         self.valid_Email = gtk.Entry()
         vbox2.add(self.valid_Email)
-
+        
         self.valid_Password = gtk.Entry()
         vbox2.add(self.valid_Password)
-
+        
         self.first_Name = gtk.Entry()
         vbox2.add(self.first_Name)
-
+        
         self.last_Name = gtk.Entry()
         vbox2.add(self.last_Name)
-
+        
         reg_frame.add(info_Box)
         main_Box.pack_start(reg_frame, False, False, 0)
 
@@ -155,27 +187,66 @@ class Aquarium_Setup:
         vbox3 = gtk.VBox(False,5)
         hbox3 = gtk.HBox(False,5)
         hbox3.add(vbox3)
-
+        
         label = gtk.Label("Intial Temperature")
         vbox3.add(label)
-        label = gtk.Label("Max Temperature")
-        vbox3.add(label)
         label = gtk.Label("Min Temperature")
+        vbox3.add(label)
+        label = gtk.Label("Max Temperature")
         vbox3.add(label)
 
         #temperature selection setting
         vbox4 = gtk.VBox(False,5)
         hbox3.add(vbox4)
 
-        adj = gtk.Adjustment(70.0, 55.0, 85.0, 1.0, 5.0, 0.0)
-        spinner = gtk.SpinButton(adj, 0, 0)
-        vbox4.add(spinner)
+        adj1 = gtk.Adjustment(70.0, 55.0, 85.0, 1.0, 5.0, 0.0)
+        self.ideal_spin = gtk.SpinButton(adj1, 0, 0)
+        vbox4.add(self.ideal_spin)
 
-        adj = gtk.Adjustment(70.0, 55.0, 85.0, 1.0, 5.0, 0.0)
-        spinner2 = gtk.SpinButton(adj, 0, 0)
-        vbox4.add(spinner2)
+        adj2 = gtk.Adjustment(70.0, 55.0, 85.0, 1.0, 5.0, 0.0)
+        self.min_spin = gtk.SpinButton(adj2, 0, 0)
+        vbox4.add(self.min_spin)
+        
+        adj3 = gtk.Adjustment(70.0, 55.0, 85.0, 1.0, 5.0, 0.0)
+        self.max_spin = gtk.SpinButton(adj3, 0, 0)
+        vbox4.add(self.max_spin)
 
-        adj = gtk.Adjustment(70.0, 55.0, 85.0, 1.0, 5.0, 0.0)
-        spinner3 = gtk.SpinButton(adj, 0, 0)
-        vbox4.add(spinner3)
+        #temperature units setting
+        vbox5 = gtk.VBox(False,5)
+        hbox3.add(vbox5)
+
+        combo = gtk.Combo()
+        combo.entry.set_text("list")
+        slist = [ "F", "C" ]
+        combo.set_popdown_strings(slist)
+        vbox5.add(combo)
+        label = gtk.Label(" ")
+        vbox5.add(label)
+        label = gtk.Label(" ")
+        vbox5.add(label)
+        
+        temp_frame.add(hbox3)
+        main_Box.pack_start(temp_frame, False, False, 0)
+
+        #cancel and submit button section
+        hbox10 = gtk.HBox(False,2)
+        main_Box.pack_start(hbox10, False,False,0)
+        
+        button = gtk.Button("Submit and Register")
+        button.connect("clicked", self.check_register)
+        hbox10.pack_end(button,False,False,2)
+        
+        button = gtk.Button("cancel")
+        button.connect_object("clicked",gtk.Widget.destroy, self.window)
+        hbox10.pack_end(button,False,False,2)
+
+        self.window.show_all()
+
+    def main(self):
+        gtk.main()
+
+
+if __name__ == "__main__":
+    Aqua_Setup = Aquarium_Setup()
+    Aqua_Setup.main()
 
